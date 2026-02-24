1f671a30d2332cb643644b32312845e673230a72
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2026-02-24 05:11:54 +0100

    cleanup build-aux

--- build-aux/pcb-revision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ROOTDIR="$(realpath "$(dirname "${0}")/../")"
-
-exec grep "(rev " "${ROOTDIR}/pcb/db-synth.kicad_pcb" | sed 's/.*rev "\([0-9.]*\)".*/\1/'
--- build-aux/version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec git describe --tags --abbrev=4 HEAD | sed -e 's/^v//' -e 's/-/./' -e 's/-g/-/'
